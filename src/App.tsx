c843bdf295c30822cae4e1d099c8c5cccf36f2a6
Brendan Norris <brendan.t.norris@gmail.com>
2023-02-16 16:05:45 -0700

    working + tailwind set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import { useState } from 'react'
 
 function App() {
-
   return (
-    <div className="App">
-      testing
-    </div>
+      <h1 className="text-3xl font-bold underline">
+          Hello world!
+      </h1>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,24 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-const appRoot = document.getElementById('root') as HTMLElement;
+const ROOT_ID = 'calculator-root';
 
-const mountApp = () => {
-    ReactDOM.createRoot(appRoot).render(
-        <React.StrictMode>
-            <App/>
-        </React.StrictMode>,
-    )
-};
-
-const waitForAppRoot = () => {
-    if (appRoot) {
-        mountApp();
-    } else {
-        setTimeout(waitForAppRoot, 100);
-    }
-};
-
-if (!appRoot) {
-    waitForAppRoot();
-}
+ReactDOM.createRoot(document.getElementById(ROOT_ID) as HTMLElement).render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
+)
