e64c24950e49fe354102d01d38af34a625d49071
Brendan Norris <brendan.t.norris@gmail.com>
2023-02-16 15:44:48 -0700

    add wait to mount

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,24 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const appRoot = document.getElementById('root') as HTMLElement;
+
+const mountApp = () => {
+    ReactDOM.createRoot(appRoot).render(
+        <React.StrictMode>
+            <App/>
+        </React.StrictMode>,
+    )
+};
+
+const waitForAppRoot = () => {
+    if (appRoot) {
+        mountApp();
+    } else {
+        setTimeout(waitForAppRoot, 100);
+    }
+};
+
+if (!appRoot) {
+    waitForAppRoot();
+}
