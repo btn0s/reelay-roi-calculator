471b8124b7ea11f585a6ff235eff5194ec7a783d
Brendan Norris <brendan.t.norris@gmail.com>
2023-02-16 16:11:39 -0700

    added tailwind build

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import { useState } from 'react'
+import {useState} from 'react'
 
 function App() {
-  return (
-      <h1 className="text-3xl font-bold underline">
-          Hello world!
-      </h1>
-  )
+    return (
+        <div className="p-6">
+            <div className="flex flex-col items-center">
+                <h1 className="text-3xl font-bold underline">
+                    Hello world!
+                </h1>
+                <p className="text-xl">
+                    This is a simple calculator.
+                </p>
+            </div>
+        </div>
+    )
 }
 
 export default App
